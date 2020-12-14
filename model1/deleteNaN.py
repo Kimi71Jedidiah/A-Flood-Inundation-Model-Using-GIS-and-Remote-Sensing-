54156f66d90ef1860fd21b87deedb025eeb6a1ec
Wenlin Gong <wenlingong@Wenlins-MacBook-Pro.local>
2020-12-14 00:41:43 -0500

    deleteNaN.py

--- model1/deleteNaN.py
+++ model1/deleteNaN.py
@@ -3,3 +3,7 @@ import pandas as pd
 
 data = pd.read_csv('DEMdelete.txt', sep=' ', error_bad_lines=False, engine='python')
 print(data.shape)
+
+for col in data.columns:
+    val = data[col].mean()
+    if()
