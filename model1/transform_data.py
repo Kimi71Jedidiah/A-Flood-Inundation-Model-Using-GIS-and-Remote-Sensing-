52cb52f44d565837a6648b9ce5491fb2facbc91c
Wenlin Gong <wenlingong@Wenlins-MacBook-Pro.local>
2020-12-14 00:33:30 -0500

    add model1 files

--- model1/transform_data.py
+++ model1/transform_data.py
@@ -2,6 +2,7 @@
 import pandas as pd
 
 data = pd.read_csv('DEMdelete.txt', sep=' ', error_bad_lines=False, engine='python')
+print(data.shape)
 for col in data.columns:
     val = data[col].mean()
     data[col] = data[col].replace(0, 9999)
